63d348ce661a3c1556b3745f2189a0bf3304f7e9
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-12 01:38:52 -0500

    commit tree ebfd0817986d9605fdc1b314bf3a108b17259918

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,19 @@
 // write your CatList component here
+
+import React, { Component } from 'react';
+
+class CatList extends Component {
+
+	renderCats = () => this.props.catPics.map(cat => <img src={cat.url} key={cat.id} width="250" height="300"/>)
+
+	render() {
+		console.log(this.props.catPics)
+		return (
+			<div>
+				{this.renderCats()}
+			</div>
+		)
+	}
+}
+
+export default CatList;
